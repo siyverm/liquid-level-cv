365f0e1a141873c358ce122649ad1d50ee9ae345
Joaquim Marcon Tavares <jmarcontavares@gmail.com>
2026-08-05 12:01:34 -0700

    finished opencv step (needs tweaking to be able to detect transparent objects)

--- download_images.py
+++ download_images.py
@@ -29,9 +29,9 @@ with open(getFileName(), mode = 'r', encoding = 'utf-8') as file:
     for row in reader:
         try:
             downloadImage(row['1_Photo_of_Container'], row['2_What_is_the_total_'], 
-                        row['3_What_is_the_volume'],index)
+                        row['3_What_is_the_height'],index)
         except requests.exceptions.RequestException as e:
             print('Download of image in row: ' + str(index) + ' failed due to ' + str(e))
         index = index + 1
 
-    print('\nFinished the download of ' + str(index) + ' images!')
+    print('\nFinished the download of ' + str(index - 2)  + ' images!')
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
+import time
+import os
+from datetime import datetime
+
+
+# Error for edge detection in cropping
+class DetectionError(Exception) :
+    pass
 
 # Using canny, crops the image passed in to its edges. 
 def crop_image_edge_detect (image) :
-    if image is None :
-        raise("Could not open or find the image")
-    
     # creates grey copy of image 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GREY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # reduce noise
     blur = cv2.GaussianBlur(gray, (5,5), 0)
@@ -19,12 +24,70 @@ def crop_image_edge_detect (image) :
     contours, _ =  cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # finds largest contour detected. 
-    if contours:
-        largest_contour = max(contours, key=cv2.contourArea)
-        x, y, w, h = cv2.boundingRect(largest_contours)
+    if not contours :
+        raise DetectionError("No Contours Found.")
     
+    largest_contour = max(contours, key=cv2.contourArea)
+    if cv2.contourArea(largest_contour) < (0.05 * image.shape[0] * image.shape[1]) : 
+        raise DetectionError("Largest Contour Too Small, likely noise.")
+
     # crops image
+    x, y, w, h = cv2.boundingRect(largest_contour)
     image = image[y:y+h, x:x+w]
+
+    return image
+
+# resizes image to make constant in training and prediciton
+def resize (image) :
+    # Shrink by the greatest amount needed to get 128 on that side
+    heightConstraint = 128 / image.shape[0]
+    widthConstraint = 128 / image.shape[1]
+
+    scaleFactor = min(widthConstraint, heightConstraint)
+    resizedImage = cv2.resize(image, None, fx = scaleFactor, fy = scaleFactor)
+
+    #Padding sides that scaled too much, ensures that proportions and size is constant. 
+    heightPadding = 128 - min(128, resizedImage.shape[0])
+    widthPadding  = 128 - min(128, resizedImage.shape[1])
+
+    topPad = heightPadding // 2
+    bottomPad = heightPadding - topPad
+    leftPad = widthPadding // 2
+    rightPad = widthPadding - leftPad
+
+    bordered = cv2.copyMakeBorder(resizedImage, topPad, bottomPad, 
+                                  leftPad, rightPad, cv2.BORDER_CONSTANT, value = (0, 0, 0))
+
+    return bordered
+
+# preprocessing function. Runs above functions and normalizes colors 0 - 1 instead of 1 - 256.
+def preprocess (imagePath) :
+    image = cv2.imread(imagePath)
+
+    if image is None :
+        raise ValueError("Could not open or find the image")
+
+    image = crop_image_edge_detect(image)
+    image = resize(image)
+    finalImage = cv2.normalize(image, None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
+    return finalImage
+
+# testing preprocess function.
+def TestImages () :
+    path = input("Path of the image you would like to test?")
+    processedImage = preprocess(path)
+
+    filename =  "Image_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+    filename = os.path.join("testImages", filename)
+
+    os.makedirs("testImages", exist_ok=True)
+    cv2.imwrite(filename, (processedImage * 255).astype(np.uint8))
+
+
+TestImages()
+
+
+
 
 
 
