bd47e2e4ead7d576e5914f81f49005a790fb8c69
siyverm <siyverma7@gmail.com>
2026-08-25 21:28:39 -0700

    Write CNN model for edge detection and prediction

--- keras_model.py
+++ keras_model.py
@@ -8,3 +8,17 @@ from matplotlib import pyplot as plt
 import keras
 from keras import layers
 from keras import ops
+
+# Initialize camera
+cap = cv2.VideoCapture(0)
+# Parsing through live video feed
+while cap.isOpened():
+    # Read frame
+    ret, frame = cap.read()
+    #Convert frame to grayscale
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #According to OpenCV it's best to blur the image for better edge detection
+    img_blur = cv2.GaussianBlur(img_gray, )
+    #parse
+    if cv2.waitKey(1) == ord('q'): #  if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
